5d5b0648308a5bc7aae41d5e57d0db9f2e29ec1a
Student <Student@PC509>
2023-08-08 15:48:46 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Nut nut = new Nut();
-        nut.getClass();
         Tree tree = new Tree();
         tree.growArrayNuts(15);
         Squirrel squirrel=new Squirrel();
